7001a5a707e3a3c26bacb1bd7bdbeb2e9d965066
chenhaoheihei <x00217230115@njit.edu.cn>
2024-03-26 21:51:10 +0800

    zuoye

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
-    public static void main(String[] args) throws Exception{
-       ConferenceEvent conferenceEvent = new ConferenceEvent("E001", "AI Conference", "Paris", "aha", 0, 0, 0, false, 0, false, 0, false, 0);
-       conferenceEvent.calculateEventCost();
-       System.out.println(conferenceEvent);
-        MusicConcertEvent event1 = new MusicConcertEvent(true, 1000.0);
-        System.out.println(event1.toString());
+    public static void main(String[] args) throws Exception {
+        ConferenceEvent conferenceEvent = new ConferenceEvent("E001", "AI Conference", "Paris", "Peng Tian", 50, 2, 25, 40, 60);
+        conferenceEvent.calculateEventCost();
+        System.out.println(conferenceEvent.toString());
 
-        MusicConcertEvent event2 = new MusicConcertEvent(false, 0.0);
-        System.out.println(event2.toString());
+        MusicConcertEvent musicConcertEvent = new MusicConcertEvent("E002", "Music Festival", "London", "Peng Tian", 50, 3, 1000);
+        musicConcertEvent.calculateEventCost();
+        System.out.println(musicConcertEvent.toString());
+
+        PastEvents pastEvent = new PastEvents("E003", "Past Event 1", "Newyork", "Peng Tian",50, 2); 
+        pastEvent.setPaymentDetails("Pending", true);      
+        System.out.println(pastEvent);
+    
     }
 }
